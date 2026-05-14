8aa84b7f7adf2d240215d250c936b6898cab2f78
Emile Silas SARE <emile.silas@gmail.com>
2026-05-14 19:07:04 +0100

    :zap: Add tests for setData and mergeData methods in RichExceptionTrait

--- tests/Traits/RichExceptionTraitTest.php
+++ tests/Traits/RichExceptionTraitTest.php
@@ -37,4 +37,45 @@ final class RichExceptionTraitTest extends TestCase
 			'line'       => $line,
 		], $e->getData(true));
 	}
+
+	public function testSetData(): void
+	{
+		$e = new RuntimeException('Test.', ['original' => 1, '_secret' => 'x']);
+
+		$result = $e->setData(['replaced' => 2]);
+
+		// fluent
+		self::assertSame($e, $result);
+
+		// original data is completely replaced
+		self::assertSame(['replaced' => 2], $e->getData(true));
+
+		// sensitive key from old data is gone
+		self::assertFalse(\array_key_exists('original', $e->getData(true)));
+		self::assertFalse(\array_key_exists('_secret', $e->getData(true)));
+	}
+
+	public function testMergeData(): void
+	{
+		$e = new RuntimeException('Test.', ['a' => 1, '_hidden' => 'h']);
+
+		$result = $e->mergeData(['b' => 2, 'a' => 99]);
+
+		// fluent
+		self::assertSame($e, $result);
+
+		// existing keys preserved / overwritten, new keys added
+		self::assertSame(99, $e->getData(true)['a']);
+		self::assertSame(2, $e->getData(true)['b']);
+
+		// sensitive key from original data is still present
+		self::assertSame('h', $e->getData(true)['_hidden']);
+
+		// sensitive keys hidden by default
+		self::assertFalse(\array_key_exists('_hidden', $e->getData()));
+
+		// non-sensitive keys still visible
+		self::assertSame(99, $e->getData()['a']);
+		self::assertSame(2, $e->getData()['b']);
+	}
 }
